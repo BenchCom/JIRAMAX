4fc87a790d0b057610cff90ee81d35c90378203b
Pavel Žifčák <pavel.zifcak@benchcom.cz>
2017-08-13 12:33:09 +0200

    get issueKey from meta by crawling through meta object

--- scripts/JiraMaX_test.js
+++ scripts/JiraMaX_test.js
@@ -1,8 +1,19 @@
 alert("JiraMax_test");
 
 // <meta name="ajs-issue-key" content="......">
-var jiraMax_issueKey = document.querySelector('meta[property="ajs-issue-key"]')['content‌​'];
+
+var jiraMax_issueKey = jiraMax_getIssueKey();
 alert(jiraMax_issueKey);
+
+jiraMax_getIssueKey() {
+	var jiraMax_metas = document.querySelector('meta');
+	for (i  = 0; i < jiraMax_metas.length; i++) {
+			if (jiraMax_metas[i].getAttribute('name') == 'ajs-issue-key') {
+				return jiraMax_metas[i].getAttribute('content');
+			}
+	}
+	return 'JIRA issueKey not found!';
+}
 
 
 
