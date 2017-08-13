78b67a3ca6606a38b0818ce30a80e6725af9dec6
Pavel Žifčák <pavel.zifcak@benchcom.cz>
2017-08-13 11:17:36 +0200

    reading content of meta

--- scripts/JiraMaX_test.js
+++ scripts/JiraMaX_test.js
@@ -1,1 +1,8 @@
-alert("No nazdar");
+alert("JiraMax_test");
+
+// <meta name="ajs-issue-key" content="......">
+var jiraMax_issueKey = document.querySelector('meta[property="ajs-issue-key"]')['content‌​'];
+alert(jiraMax_issueKey);
+
+
+
