722783c6c12ee76fdfad8561806b419129382c70
Pavel Žifčák <pavel.zifcak@benchcom.cz>
2017-08-13 14:40:10 +0200

    authKey encoding

--- scripts/JiraMaX_test.js
+++ scripts/JiraMaX_test.js
@@ -1,22 +1,34 @@
 alert("JiraMax_test");
 
-// <meta name="ajs-issue-key" content="......">
-
-var jiraMax_issueKey = jiraMax_getIssueKey();
-alert(jiraMax_issueKey);
-
-function jiraMax_getIssueKey() {
-	var jiraMax_metas = document.getElementsByTagName('meta');
-    // alert(jiraMax_metas);
-    // alert(jiraMax_metas.length);
-	for (i  = 0; i < jiraMax_metas.length; i++) {
-    		// alert(jiraMax_metas[i].getAttribute('name'));
-			if (jiraMax_metas[i].getAttribute('name') == 'ajs-issue-key') {
-				return jiraMax_metas[i].getAttribute('content');
-			}
-	}
-	return 'JIRA issueKey not found!';
+var issueKey = getIssueKey();
+if (issueKey == '') {
+	alert(issueKey);
+} else {
+	alert('JIRA issueKey not found!');
+	return false;
 }
 
+var macoHost = '';
+var userName = prompt('User Name');
+var password = prompt('Password');
+var authKey = btoa(userName + ':' + password);
+
+alert(authKey);
+
+/*
+initJobBudgetLines();
+createJobBudgetLines();
+*/
+
+function getIssueKey() {
+	// <meta name="ajs-issue-key" content="......">
+	var metas = document.getElementsByTagName('meta');
+	for (i = 0; i < metas.length; i++) {
+		if (metas[i].getAttribute('name') == 'ajs-issue-key') {
+			return metas[i].getAttribute('content');
+		}
+	}
+	return '';
+}
 
 
