bcdb7728d12d13a18d57f1ce5d456a4a51903a57
Pavel Žifčák <pavel.zifcak@benchcom.cz>
2017-08-13 17:53:54 +0200

    ui & flow refactored

--- scripts/JiraMaX_test.js
+++ scripts/JiraMaX_test.js
@@ -1,23 +1,43 @@
 (function() {
 
-	alert("JiraMax_test");
-
 	var issueKey = getIssueKey();
-	if (issueKey != '') {
-		alert(issueKey);
-	} else {
+	if (issueKey == '') {
 		alert('JIRA issueKey not found!');
-		return;
+		return;	
 	}
-
-	var macoHost = '';
+	
+	var macoHost = 'http://192.168.1.170:8082/containers/v1/prod/';
+	var jbl = '/jobbudgetlinedependencies/';
 	var userName = prompt('User Name');
 	var password = prompt('Password');
 	var authKey = btoa(userName + ':' + password);
+	//alert(authKey);
 
-	alert(authKey);
+	var jobNumber = getJobNumber();
+	var taskName = issueKey.substring(issueKey.indexOf('-') + 1);
+	
+	if (sumLineExists()) {
+		alert('Sum Line with TaskName' + taskName + ' exists already.');
+		return;
+	}
+	
+	if (!confirm('Job Budget Lines do not exist yet for ' + issueKey + ' ticket.\n' 
+			   + 'Do you want to create them now?') {
+		return;
+	};
+	
 
-	var job = getJobNumber();
+	/*
+	 * todo:
+	 * check if sumline exists by filter
+	 * create new lines with jobbudgetlinedependencies/card
+	 * 
+	 */
+	
+
+/*
+ * function definitions follow
+ */
 
 	function getIssueKey() {
 		// <meta name="ajs-issue-key" content="......">
@@ -33,7 +53,6 @@
 	function getJobNumber() {
 		var jiraProject = issueKey.substring(0, issueKey.indexOf('-'));
 		if (jiraProject.length > 0) {
-			alert(jiraProject);
 			switch (jiraProject) {
 				case 'HDASB': return 'ASB';
 				case 'HDASE': return 'ASE';
@@ -43,6 +62,19 @@
 			}
 		}
 		return '';
+	}
+	
+	function sumLineExists() {
+		    var xhttp = new XMLHttpRequest();
+		    xhttp.open('POST', macoHost, true);
+		    xhttp.setRequestHeader('Content-type', 'application/json');
+		    xhttp.setRequestHeader('Accept', 'application/json');
+		    xhttp.setRequestHeader('Accept-language', 'en-US');
+		    xhttp.setRequestHeader('Authorization', 'Basic ' + authKey);
+		    xhttp.setRequestHeader('Maconomy-Authentication', 'X-Basic, X-Force-Maconomy-Credentials');
+		    xhttp.send();
+		    var response = JSON.parse(xhttp.responseText);
+		return true;
 	}
 
 })();
